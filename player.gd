8c9d5d36cfd45f103beffa690ad6e8f5d794ff88
Nicholas Peters <thenootlinja@gmail.com>
2026-02-19 12:49:52 -0500

    Add script to player scene.

---
+++ player.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@export var speed = 400 # How fast that player will move in pixels/sec
+var screen_size # Game window size
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
